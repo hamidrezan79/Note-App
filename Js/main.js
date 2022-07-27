b7f3f3a32fa417483002414ce6a7bb9e48ec1f67
hamidrezan79 <hamidrezanaderian8@gmail.com>
2022-07-27 18:09:37 +0430

    final change to note list

--- Js/main.js
+++ Js/main.js
@@ -1,20 +1,7 @@
-import notesAPI from "./NotesAPI.js";
-import NotesView from "./NotesView.js";
+import App from "./App.js";
 
-const app = document.getElementById("app");
 
-const view = new NotesView(app, {
-    onNoteAdd() {
-        console.log("note has been added");
-    },
-    onNoteEdit(newTitle, newBody) {
-        console.log(newBody, newTitle);
-    },
-    onNoteSelect(noteId){
-        console.log(noteId);
-    },
-    onNoteDelete(noteId){
-        console.log(noteId);
-    }
-});
-view.updateNoteList(notesAPI.getAllNotes());
+const root = document.getElementById("app");
+const app  = new App(root)
+
+
